bacf60858815de333d5f6b105f0e34756b163d5c
Mariam <mariam.martirosyan26@gmail.com>
2024-01-07 14:12:25 +0400

    new commit

---
+++ Lesson1.js
@@ -0,0 +1,6 @@
+test('basic test', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    const title = page.locator('.navbar__inner .navbar__title');
+    await expect(title).toHaveText('Playwright');
+    
+  });
